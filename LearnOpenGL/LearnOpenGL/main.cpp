aeb1c32f6e47877a9cbeddce3822721364cd1bb7
puikiri <1554398165@qq.com>
2021-03-28 16:11:14 +0800

    整理代码2

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -22,36 +22,21 @@ float vertices1[] = {
 	0.f, -0.7f, 0.0f, 0.0f, 0.0f, 1.0f,
 };
 
-//float vertices2[] = {
-//	0.8f, 0.8f, 0.0f, 0.0f, 0.0f, 1.0f,
-//	0.8f, 0.7f, 0.0f, 0.0f, 1.0f, 0.0f,
-//	0.7f, 0.7f, 0.0f, 1.0f, 0.0f, 0.0f,
-//	0.7f, 0.8f, 0.0f, 0.5f, 0.5f, 0.5f
-//};
-//
-//unsigned int vertices2indices[] = {
-//	0, 1, 3,
-//	1, 2, 3,
-//	0, 2, 3
-//};
-
-
+unsigned int vertices1indices[] = {
+	0, 1, 2,
+	3, 4, 5,
+};
 
 int main()
 {
 	Render render;
 
 	std::shared_ptr<Shader> shader = std::make_shared<Shader>("../LearnOpenGL/resource/shader/ShiningDelta.vs", "../LearnOpenGL/resource/shader/ShiningDelta.fs");
-	std::shared_ptr<RenderObject> ro1 = std::make_shared<RenderObject>(shader);
-	ro1->createVBORenderObject(vertices1, sizeof(vertices1), 0, 3, false, 6, 0);
-	ro1->attachVertexAttribPointer(1, 3, false, 6, 3);
-	ro1->setDrawVerNum(6);
-	/*std::shared_ptr<RenderObject> ro2 = std::make_shared<RenderObject>(shader);
-	ro2->createEBORenderObject(vertices2, sizeof(vertices2), vertices2indices, sizeof(vertices2indices), 0, 3, false, 6, 0);
+	std::shared_ptr<RenderObject> ro2 = std::make_shared<RenderObject>(shader);
+	ro2->createEBORenderObject(vertices1, sizeof(vertices1), vertices1indices, sizeof(vertices1indices), 0, 3, false, 6, 0);
 	ro2->attachVertexAttribPointer(1, 3, false, 6, 3);
-	ro2->setDrawVerNum(9);*/
-	render.gerRenderWorld()->regRenderObject(ro1->getName(), ro1);
-	//render.gerRenderManager()->regRenderObject(ro2->getName(), ro2);
+	ro2->setDrawVerNum(6);
+	render.gerRenderWorld()->regRenderObject(ro2->getName(), ro2);
 	while (checkLoop())
 	{
 		float timeValue = glfwGetTime();
